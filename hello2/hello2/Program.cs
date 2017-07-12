1c0c74d038c10990da503cc82032827c70c091fc
WycliffBwana <weakleafb@gmail.com>
2017-07-12 13:28:44 +0300

    Hello from 2

---
+++ hello2/hello2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from hello 2");
+            Console.ReadLine();
+        }
+    }
+}
